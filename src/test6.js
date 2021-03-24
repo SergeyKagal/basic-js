031017e75e81d3412a93d62d295d3eed5b12bf51
SergeyKagal <3461995@gmail.com>
2021-03-24 05:17:02 +0300

    feat: 7/10 done

--- src/test6.js
+++ src/test6.js
@@ -1,23 +1,65 @@
+
+
 const  myObject =
 {
     items : [],
-    sayHi() {console.log('Hello')},
-    sayBue() {console.log('Good Bue!')},
-    
-    addItem(value) 
+
+       addItem(value) 
     {
-        if(!value)this.items.push('(  )');
-        else this.items.push('( '+value.toString()+' )');
-    return this;
-    }
+       
+         this.items.push(`( ${value} )`);
+    
+    },
+
+    getLength() {
+        return this.items.length;
+        },
 
 
+    removeLink(position) {       
+        if(typeof(position)=='number' && position>0 && position<(this.items.length+1) && position%1==0)
+        this.items.splice((position-1),1);
+        else
+         {  this.items=[];
+             throw new Error;
+            }
+
+        },
+
+    reverseChain() 
+    {
+    this.items.reverse();
+    },
+
+    finishChain() {
+        let resulChain = this.items.join('~~');
+        this.items=[];
+       console.log(resulChain);  
+        return resulChain;  
+      }
 
 }
 
 
-myObject.sayHi();
-myObject.sayBue();
 
-myObject.addItem();
+
+myObject.addItem(1);
+myObject.addItem(2);
+myObject.addItem(3);myObject.addItem(4);myObject.addItem(5);myObject.addItem(6);
+myObject.addItem(function () {});
+
+
 console.log(myObject.items);
+myObject.removeLink(1);
+myObject.reverseChain();
+
+console.log(myObject.getLength());
+myObject.finishChain();
+
+
+
+
+console.log(myObject.items);
+console.log((` ${function () {}} `));
+console.log(typeof(function () {}));
+
