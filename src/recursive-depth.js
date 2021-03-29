b44d5b832f4eea07551c558d0ab136395adf2d39
SergeyKagal <78958096+SergeyKagal@users.noreply.github.com>
2021-03-29 22:56:11 +0200

    feat: basic-js end

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(array, cnt=1) {
-    if (!Array.isArray(array)) throw new Exception('not an array');
     
     var result = cnt;
 
