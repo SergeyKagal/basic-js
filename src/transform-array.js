615d157960414972a49bd075e52763af6bb45969
Sergey Kagal <78958096+SergeyKagal@users.noreply.github.com>
2021-09-10 20:34:13 +0300

    feat: add trans array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+ if (!Array.isArray(arr)) throw new Error;
+
+  var resultArr=[...arr];
+  var ctrl=['--double-next','--discard-next', '--double-prev','--discard-prev'];
+  
+  for(var i=0;i<resultArr.length;i++){
+  
+      if(resultArr[i]==ctrl[0] && i<(resultArr.length-1)) resultArr.splice(i,1,resultArr[i+1]);
+      if(resultArr[i]==ctrl[1] && i<(resultArr.length-1)) resultArr.splice(i+1,1);
+      if(resultArr[i]==ctrl[2] && i>0) resultArr.splice(i,1,resultArr[i-1]);
+      if(resultArr[i]==ctrl[3] && i>0) resultArr.splice(i-1,1);
+  }
+ 
+  for(var i=0;i<ctrl.length;i++) resultArr=resultArr.filter(elem => elem!=ctrl[i]);
+  //console.log(arr);
+  //console.log(resultArr);
+  return resultArr;
 }
