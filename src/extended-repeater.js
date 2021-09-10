451c50a9495ceaf51692e788f08f52df4a9c2c82
Sergey Kagal <78958096+SergeyKagal@users.noreply.github.com>
2021-09-10 09:08:08 +0300

    feat: extended repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater( str, options ) {
+   let result='';
+  let resString=(`${str}`);
+  
+  if(options.addition===null) options.addition='null';
+if(!options.repeatTimes) options.repeatTimes = 1;
+if(!options.separator) options.separator='+';
+if(!options.additionSeparator) options.additionSeparator='|';
+if(!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+if(options.addition==undefined) options.addition="";
+
+
+
+    for(var i=0;i<options.repeatTimes;i++)
+    {
+      result+=resString;
+      for(var j=0;j<options.additionRepeatTimes;j++)
+      {
+        result+=options.addition;
+        if (j<(options.additionRepeatTimes-1)) result+=options.additionSeparator;
+      }
+      if(i<options.repeatTimes-1)
+      result+=options.separator;
+    }    
+      return result;
+ // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
