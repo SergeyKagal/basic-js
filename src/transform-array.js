a7ee6051d6a10e48be5040f99a97ccb0133975ab
SergeyKagal <3461995@gmail.com>
2021-10-01 01:07:07 +0300

    fix: add errors message

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,20 +14,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function transform(arr) {
- if (!Array.isArray(arr)) throw new Error;
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
 
-  var resultArr=[...arr];
-  var ctrl=['--double-next','--discard-next', '--double-prev','--discard-prev'];
-  
-  for(var i=0;i<resultArr.length;i++){
-  
-      if(resultArr[i]==ctrl[0] && i<(resultArr.length-1)) resultArr.splice(i,1,resultArr[i+1]);
-      if(resultArr[i]==ctrl[1] && i<(resultArr.length-1)) resultArr.splice(i+1,1);
-      if(resultArr[i]==ctrl[2] && i>0) resultArr.splice(i,1,resultArr[i-1]);
-      if(resultArr[i]==ctrl[3] && i>0) resultArr.splice(i-1,1);
+  var resultArr = [...arr];
+  var ctrl = ['--double-next', '--discard-next', '--double-prev', '--discard-prev'];
+
+  for (var i = 0; i < resultArr.length; i++) {
+
+    if (resultArr[i] == ctrl[0] && i < (resultArr.length - 1)) resultArr.splice(i, 1, resultArr[i + 1]);
+    if (resultArr[i] == ctrl[1] && i < (resultArr.length - 1)) resultArr.splice(i + 1, 1);
+    if (resultArr[i] == ctrl[2] && i > 0) resultArr.splice(i, 1, resultArr[i - 1]);
+    if (resultArr[i] == ctrl[3] && i > 0) resultArr.splice(i - 1, 1);
   }
- 
-  for(var i=0;i<ctrl.length;i++) resultArr=resultArr.filter(elem => elem!=ctrl[i]);
+
+  for (var i = 0; i < ctrl.length; i++) resultArr = resultArr.filter(elem => elem != ctrl[i]);
   //console.log(arr);
   //console.log(resultArr);
   return resultArr;
--- src/what-season.js
+++ src/what-season.js
@@ -11,13 +11,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason( d ) {
- if(d==null)return 'Unable to determine the time of year!';
-  if(Object.prototype.toString.call(d)!='[object Date]') throw new Error ;
- 
-var monthNum=d.getMonth();
-if(monthNum>=0 && monthNum<2 || monthNum ==11) return 'winter';
-else if (monthNum>1 && monthNum<5) return 'spring';
-else if (monthNum>4 && monthNum<8) return 'summer';
-else if (monthNum>7 && monthNum<11) return 'autumn';
+export default function getSeason(d) {
+  if (d == null) return 'Unable to determine the time of year!';
+  if (toString.call(d) != '[object Date]' || Object.keys(d).length > 0) throw new Error('Invalid date!');
+
+  var monthNum = d.getMonth();
+  if (monthNum >= 0 && monthNum < 2 || monthNum == 11) return 'winter';
+  else if (monthNum > 1 && monthNum < 5) return 'spring';
+  else if (monthNum > 4 && monthNum < 8) return 'summer';
+  else if (monthNum > 7 && monthNum < 11) return 'autumn';
 }
